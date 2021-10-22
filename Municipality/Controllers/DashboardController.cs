b6378f8fb3ba9779bee491d7323d8c56da386234
Amirmohammad <mirjalili16@gmail.com>
2021-10-22 20:23:49 +0330

    Add project files.

---
+++ Municipality/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Municipality.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Average() => View();
+    }
+}
